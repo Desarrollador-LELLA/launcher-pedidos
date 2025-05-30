6ae04ad5944fe953fcbd294014c84a7b3dd06dc2
Luis Esteban Llancamil Aguilera <luis.llancamil.a@gmail.com>
2025-05-30 02:25:52 -0400

    Actualizacion
    versión 0.2.1

--- src/launcher/pedidos/LauncherPedidos.java
+++ src/launcher/pedidos/LauncherPedidos.java
@@ -13,7 +13,7 @@ import java.util.jar.Manifest;
  * @author NOTEJUAN
  */
 public class LauncherPedidos {
-    // TESTER VERSION
+    // VERSION 0.2.1
     public static void main(String[] args) {
         try {
             String destinationDir = "app";
